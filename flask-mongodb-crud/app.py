fb7dcf6f176f611254e66e6963e6804a6d34528a
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-02-04 09:10:09 +0500

    flask crud

--- flask-mongodb-crud/app.py
+++ flask-mongodb-crud/app.py
@@ -1,23 +1,63 @@
 # flask, pymongo
-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from pymongo import MongoClient
+from bson import ObjectId
 
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["lms_2306b1"]
 students_collection = db["students"]
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # logic to fetch data from mongodb collection
 
-@app.route("/add-student")
-def hello_world():
+    students_list = list(students_collection.find())
+
+    return render_template("index.html", 
+                           students_list=students_list)
+
+@app.route("/add-student", methods=["GET", "POST"])
+def addStudent():
+
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        contact = request.form['contact']
+        course = request.form['course']
+        students_collection.insert_one({"name": name, 
+                                        "email": email, 
+                                        "contact": contact, 
+                                        "course": course})
+        return redirect(url_for("index"))
+
     return render_template("add-student.html")
+
+@app.route("/delete-student/<id>")
+def deleteStudent(id):
+    students_collection.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('index'))
+
+@app.route("/edit-student/<id>", methods=["GET", "POST"])
+def editStudent(id):
+    student = students_collection.find_one({"_id": ObjectId(id)})
+
+    return render_template("edit-student.html", student=student)
+
+
+# @app.route("/submit-student", methods=["POST"])
+# def submitStudent():
+#     name = request.form['name']
+#     email = request.form['email']
+#     contact = request.form['contact']
+#     course = request.form['course']
+#     students_collection.insert_one({"name": name, 
+#                                     "email": email, 
+#                                     "contact": contact, 
+#                                     "course": course})
+#     return redirect(url_for("index"))
 
 
 
